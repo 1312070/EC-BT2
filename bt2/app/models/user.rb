7dfcaba15970d7a7309093179263c88dd63162df
1312071 <1312071@student.hcmus.edu.vn>
2016-10-11 12:03:39 +0700

    update v1

--- bt2/app/models/user.rb
+++ bt2/app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+	email_format = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+	#validation:
+	validates :username, :password, :email, :presence => true
+	validates :username, :length => {:minimum => 3}
+	validates :username, :uniqueness => {:message => "username already taken"}
+	validates :password, :length => {:minimum => 5}
+	validates :email, :uniqueness => {:message => "email already taken"}
 end
